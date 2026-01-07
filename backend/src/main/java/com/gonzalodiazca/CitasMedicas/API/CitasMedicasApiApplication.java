49cdb8cc82bfacd4c65522de4f4795fed3935319
gonzalodiazcanavate <gonzalodiazca@gmail.com>
2026-01-07 18:36:46 +0100

    feat: Estructura inicial del backend.

---
+++ backend/src/main/java/com/gonzalodiazca/CitasMedicas/API/CitasMedicasApiApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalodiazca.CitasMedicas.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasMedicasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasMedicasApiApplication.class, args);
+	}
+
+}
